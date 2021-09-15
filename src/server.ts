4530af7f09efe529e4db8c9e67ea0162256075fe
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-15 16:24:06 -0300

    fix: post route removed

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,4 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
 });
 
-app.post('/acount', (request, response) => {
-  const { name } = request.body;
-
-  return response.status(201).send({ message: 'Customer created!' });
-});
-
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}!`));
