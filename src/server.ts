b5b520551c43defa256a527170bf5f6f0267705e
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-15 18:09:41 -0300

    feat: add library uuid to create id category

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
+const PORT = 3333;
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
-
-const PORT = 3333;
+/**
+ * Not to be repetitive, just once the route you want to access.
+ * So, just declare the path that will be accessed in "routes"
+ */
+app.use('/categories', categoriesRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}!`));
