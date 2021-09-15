f78e0ebafc1599a1db48444514ccadbd77b6e9f3
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-15 16:22:30 -0300

    build: configs to debug code implemented

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3333;
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
+});
+
+app.post('/acount', (request, response) => {
+  const { name } = request.body;
+
+  return response.status(201).send({ message: 'Customer created!' });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}!`));
