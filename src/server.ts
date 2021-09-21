34dd711c4436d4312fa72ab8b78160a4bca3ccac
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-21 16:03:51 -0300

    fix: removed comments

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ const PORT = 3333;
 
 app.use(express.json());
 
-/**
- * Not to be repetitive, just once the route you want to access.
- * So, just declare the path that will be accessed in "routes"
- */
 app.use('/categories', categoriesRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}!`));
